08db3a8347de8ebe469f577ee1b9eda86e706946
Luis Orellana <Luis.Orellana@solera.com>
2025-01-21 10:58:26 +0100

    feat: implement Game class with the initial constructor and add validation tests

---
+++ src/FootballScoreBoard/Game.cs
@@ -0,0 +1,30 @@
+﻿namespace FootballScoreBoard;
+
+public class Game
+{
+    public string Id { get; }
+    public string HomeTeam { get; }
+    public string AwayTeam { get; }
+    public int HomeScore { get; private set; }
+    public int AwayScore { get; private set; }
+    public DateTime? StartTime { get; private set; }
+
+    public Game(string homeTeam, string awayTeam)
+    {
+        if (string.IsNullOrWhiteSpace(homeTeam) || string.IsNullOrWhiteSpace(awayTeam))
+        {
+            throw new ArgumentException("Team names cannot be empty");
+        }
+
+        HomeTeam = homeTeam;
+        AwayTeam = awayTeam;
+        Id = GetGameId(homeTeam, awayTeam);
+        HomeScore = 0;
+        AwayScore = 0;
+    }
+
+    private static string GetGameId(string homeTeam, string awayTeam)
+    {
+        return $"{homeTeam}|{awayTeam}";
+    }
+}
---
+++ tests/FootballScoreBoard.Tests/UnitTests/GameTests.cs
@@ -0,0 +1,39 @@
+﻿namespace FootballScoreBoard.Tests.UnitTests;
+
+public class GameTests
+{
+    [Fact]
+    public void Game_ShouldInitialize_WithCorrectDefaultValues()
+    {
+        // Act
+        var game = new Game("Mexico", "Canada");
+
+        // Assert
+        Assert.Equal("Mexico", game.HomeTeam);
+        Assert.Equal("Canada", game.AwayTeam);
+        Assert.Equal("Mexico|Canada", game.Id);
+        Assert.Equal(0, game.HomeScore);
+        Assert.Equal(0, game.AwayScore);
+        Assert.Null(game.StartTime);
+    }
+
+    [Theory]
+    [MemberData(nameof(InvalidTeamNames))]
+    public void Game_ShouldThrowArgumentException_WhenHomeTeamOrAwayTeamIsInvalid(string homeTeam, string awayTeam)
+    {
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => new Game(homeTeam, awayTeam));
+        Assert.Equal("Team names cannot be empty", exception.Message);
+    }
+
+    public static IEnumerable<object[]> InvalidTeamNames()
+    {
+        string[] invalidTeamNames = [null, string.Empty, " ", "  ", "\n", "\t"];
+
+        foreach (var invalidTeamName in invalidTeamNames)
+        {
+            yield return new object[] { "Mexico", invalidTeamName };
+            yield return new object[] { invalidTeamName, "Canada" };
+        }
+    }
+}
